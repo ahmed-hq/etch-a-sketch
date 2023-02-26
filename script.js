296405d7cf3506ee3303e3b07074becd5683aa84
Ahmed Walid <123060771+ahmed-hq@users.noreply.github.com>
2023-02-26 09:00:26 +0200

    Ability to Inc. Squares Num

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
 let gridContainer = document.querySelector(".grid-container");
 
+let num = 150;
 
-function makeRsAndCols (rows, cols){
-    gridContainer.style.setProperty('--grid-rows', rows);
-    gridContainer.style.setProperty('--grid-cols', cols);
-    for(let c = 0; c < (rows * cols); c++){
+
+function makeRsAndCols (){
+    gridContainer.style.setProperty('--grid-rows', num);
+    gridContainer.style.setProperty('--grid-cols', num);
+    for(let c = 0; c < (num * num); c++){
         let cell = document.createElement('div');
         // cell.innerText = (c + 1);
         gridContainer.appendChild(cell).className = "grid-item hover";
     };
 };
 
-makeRsAndCols(16, 16);
+//number of squares per side 
+// function askForNum (num){
+//     let num = prompt("Write Numbers of Squares"); 
+// }
+
+makeRsAndCols();
+
