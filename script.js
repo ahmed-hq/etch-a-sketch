3261a6f82995f6769ffcd04e59ea16e71843055c
Ahmed Walid <123060771+ahmed-hq@users.noreply.github.com>
2023-02-26 08:20:14 +0200

    Adding Hover Effect

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 let gridContainer = document.querySelector(".grid-container");
 
+
 function makeRsAndCols (rows, cols){
     gridContainer.style.setProperty('--grid-rows', rows);
     gridContainer.style.setProperty('--grid-cols', cols);
     for(let c = 0; c < (rows * cols); c++){
         let cell = document.createElement('div');
         // cell.innerText = (c + 1);
-        gridContainer.appendChild(cell).className = "grid-item";
+        gridContainer.appendChild(cell).className = "grid-item hover";
     };
 };
 
